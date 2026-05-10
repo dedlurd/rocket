e8c2e0db25d64e4da06b634f98bad05a2c85ae1d
Dedlurd <olopadefavour57@gmail.com>
2026-05-10 08:17:15 +0100

    feat: enhance mobile navigation and build process
    - Updated package.json to streamline Tailwind CSS build commands and added a watch script.
    - Modified index.html to include a mobile menu and a script for interactivity.
    - Created main.js to handle mobile menu toggle functionality.
    - Enhanced output.css with new utility classes and responsive design adjustments for mobile navigation.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const initApp = ( ) => {
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobilemenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = () => {
+        mobilemenu.classList.toggle('hidden');
+        mobilemenu.classList.toggle('flex');
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobilemenu.addEventListener('click', toggleMenu);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
